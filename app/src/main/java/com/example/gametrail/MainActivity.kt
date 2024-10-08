3e56030305a6ae4bdcc985b2ec971e548ae7dae2
zane-a-bit <zanenorth96@gmail.com>
2024-10-08 19:59:23 +0300

    lining up the main activity.kt to the activity_main.xml

--- app/src/main/java/com/example/gametrail/MainActivity.kt
+++ app/src/main/java/com/example/gametrail/MainActivity.kt
@@ -17,31 +17,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            GameTrailTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GameTrailTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
